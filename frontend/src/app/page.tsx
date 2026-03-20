4c361f97d1133fce18cf8f4e1a109cd8220917e4
wcqqq1214 <wcqqq1214@gmail.com>
2026-03-20 22:29:18 +0800

    refactor: update Home page to use AssetSelector with new props

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -15,21 +15,23 @@ export default function Home() {
       <div className="flex-1 flex flex-col gap-4 overflow-hidden">
         {/* Top: Stock selector (40% height) */}
         <div className="h-[40%] overflow-y-auto">
-          <StockSelector
-            selectedStock={selectedStock}
-            onStockSelect={setSelectedStock}
+          <AssetSelector
+            selectedAsset={selectedAsset}
+            onAssetSelect={setSelectedAsset}
+            assetType={assetType}
+            onAssetTypeChange={setAssetType}
           />
         </div>
 
         {/* Bottom: K-line chart (60% height) */}
         <div className="flex-1 overflow-hidden">
-          <KLineChart selectedStock={selectedStock} />
+          <KLineChart selectedStock={selectedAsset} />
         </div>
       </div>
 
       {/* Right panel: Chat */}
       <div className="w-[35%] border-l overflow-hidden flex flex-col">
-        <ChatPanel selectedStock={selectedStock} />
+        <ChatPanel selectedStock={selectedAsset} />
       </div>
     </div>
   );
